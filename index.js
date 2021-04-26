6e52395046d74fb37db3b4d38aa8efb0b42510be
unknown <arshadqadri321@gmail.com>
2021-04-26 09:35:32 +0530

    added search functionality with country name

--- index.js
+++ index.js
@@ -1,37 +1,75 @@
-const conutry_name = document.querySelector('#conutry_name');
-const update = document.querySelector('#update');
-const NewConfirmed = document.querySelector('#NewConfirmed');
-const NewDeaths = document.querySelector('#NewDeaths');
-const NewRecovered = document.querySelector('#NewRecovered');
-const TotalConfirmed = document.querySelector('#TotalConfirmed');
-const TotalDeaths = document.querySelector('#TotalDeaths');
-const TotalRecovered = document.querySelector('#TotalRecovered');
-const input = document.querySelector('#input');
+const Global = document.querySelector('#Global');
+const Gupdate = document.querySelector('#Gupdate');
+const GNewConfirmed = document.querySelector('#GNewConfirmed');
+const GNewDeaths = document.querySelector('#GNewDeaths');
+const GNewRecovered = document.querySelector('#GNewRecovered');
+const GTotalConfirmed = document.querySelector('#GTotalConfirmed');
+const GTotalDeaths = document.querySelector('#GTotalDeaths');
+const GTotalRecovered = document.querySelector('#GTotalRecovered');
+
+// -------------------------------------------------------------
+
 const myserach = document.querySelector('#serach');
+const countryData = document.querySelector('#country');
 let realData = '';
-let x = 76;
-const getNewCovidCases = () => {
-  x = realData.Countries.findIndex(e => e.Country === input.value);
-  console.log(`x value ${x}`);
-  const arrayData = realData.Countries[x];
-  conutry_name.innerHTML = `Country : ${arrayData.Country} (${arrayData.CountryCode})`;
-  update.innerHTML = `<b> Last Update : </b> ${arrayData.Date} `;
-  NewConfirmed.innerHTML = arrayData.NewConfirmed;
-  NewDeaths.innerHTML = arrayData.NewDeaths;
-  NewRecovered.innerHTML = arrayData.NewRecovered;
-  TotalConfirmed.innerHTML = arrayData.TotalConfirmed;
-  TotalDeaths.innerHTML = arrayData.TotalDeaths;
-  TotalRecovered.innerHTML = arrayData.TotalRecovered;
-  console.log(realData);
-  console.log(input.value);
-  console.log(x);
+
+const getGlobalCases = () => {
+  const GarrayData = realData.Global;
+  Global.innerHTML = 'Global';
+  Gupdate.innerHTML = `<b> Last Update : </b> ${GarrayData.Date} `;
+  GNewConfirmed.innerHTML = GarrayData.NewConfirmed;
+  GNewDeaths.innerHTML = GarrayData.NewDeaths;
+  GNewRecovered.innerHTML = GarrayData.NewRecovered;
+  GTotalConfirmed.innerHTML = GarrayData.TotalConfirmed;
+  GTotalDeaths.innerHTML = GarrayData.TotalDeaths;
+  GTotalRecovered.innerHTML = GarrayData.TotalRecovered;
 };
+
+const getNewCovidCases = () => {
+  let x = realData.Countries.findIndex(e => e.Country === input.value);
+  const arrayData = realData.Countries[x];
+  if (x >= 0) {
+    countryData.innerHTML = ` <div class="d-flex justify-content-between my-4">
+    <h5>  Country : <b> ${arrayData.Country} </b></h5>
+    <div> <b> Last Update : </b> ${arrayData.Date} </div>
+  </div>
+  <table class="table table-responsive shadow">
+    <thead class="bg-dark text-white">
+      <tr>
+        <th scope="col">New Confirmed</th>
+        <th scope="col">New Deaths</th>
+        <th scope="col">New Recovered</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td> ${arrayData.NewConfirmed} </td>
+        <td> ${arrayData.NewDeaths} </td>
+        <td> ${arrayData.NewRecovered} </td>
+      </tr>
+      <tr>
+        <th scope="col">Total Confirmed</th>
+        <th scope="col">Total Deaths</th>
+        <th scope="col">Total Recovered</th>
+      </tr>
+      <tr>
+        <td> ${arrayData.TotalConfirmed} </td>
+        <td> ${arrayData.TotalDeaths} </td>
+        <td> ${arrayData.TotalRecovered} </td>
+      </tr>
+    </tbody>
+  </table>`;
+  }
+};
+
 const getCovidCases = async () => {
   const api = 'https://api.covid19api.com/summary';
   try {
     const data = await fetch(api);
     realData = await data.json();
     console.log(realData.Countries);
+    console.log(realData.Global);
+    getGlobalCases();
     getNewCovidCases();
   } catch (error) {}
 };
